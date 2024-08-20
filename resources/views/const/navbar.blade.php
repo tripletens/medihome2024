b779b01d1b1dff6c4e812519e17911121e45acb8
User <tripletens.kc@gmail.com>
2024-08-20 16:23:08 +0100

    fix the navbar

--- resources/views/const/navbar.blade.php
+++ resources/views/const/navbar.blade.php
@@ -1,15 +1,16 @@
 <nav class="flex items-center justify-between flex-wrap p-6 fixed w-full z-10 top-0 mb-6" x-data="{ isOpen: false }"
-    @keydown.escape="isOpen = false" :class="{ 'shadow-lg bg-indigo-900': isOpen, 'bg-gray-800': !isOpen }">
-    <!--Logo etc-->
+    @keydown.escape="isOpen = false" :class="{ 'shadow-lg': isOpen }"
+    :style="isOpen ? 'background-color: #2F5E99;' : 'background-color: #2F5E99;'">
+    {{--  <!--Logo etc--> 2F5E99  --}}
     <div class="flex items-center flex-shrink-0 text-white mr-6">
         <a class="text-white no-underline hover:text-white hover:no-underline" href="#">
-            <span class="text-2xl pl-2"><i class="em em-grinning"></i> Brand McBrandface</span>
+            <span class="text-2xl pl-2"><i class="em em-grinning"></i> Medihomecare</span>
         </a>
     </div>
 
     <!--Toggle button (hidden on large screens)-->
     <button @click="isOpen = !isOpen" type="button"
-        class="block lg:hidden px-2 text-gray-500 hover:text-white focus:outline-none focus:text-white"
+        class="block lg:hidden px-2 text-white hover:text-white focus:outline-none focus:text-white"
         :class="{ 'transition transform-180': isOpen }">
         <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path x-show="isOpen" fill-rule="evenodd" clip-rule="evenodd"
@@ -25,21 +26,22 @@
         x-show.transition="true">
         <ul class="pt-6 lg:pt-0 list-reset lg:flex justify-end flex-1 items-center">
             <li class="mr-3">
-                <a class="inline-block py-2 px-4 text-white no-underline" href="{{ route('landing-page') }}" @click="isOpen = false">Home
+                <a class="inline-block py-2 px-4 text-white no-underline" href="{{ route('landing-page') }}"
+                    @click="isOpen = false">Home
                 </a>
             </li>
             <li class="mr-3">
-                <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+                <a class="inline-block text-blue-200 no-underline border-b-2 border-transparent hover:border-blue-300 hover:text-blue-200 hover:text-underline py-2 px-4"
                     href="{{ route('about-page') }}" @click="isOpen = false">About
                 </a>
             </li>
             <li class="mr-3">
-                <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+                <a class="inline-block text-blue-200 no-underline border-b-2 border-transparent hover:border-blue-300  hover:text-blue-200 py-2 px-4"
                     href="{{ route('testimonial-page') }}" @click="isOpen = false">Testimonial
                 </a>
             </li>
             <li class="mr-3">
-                <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+                <a class="inline-block text-blue-200 no-underline border-b-2 border-transparent hover:border-blue-300 hover:text-blue-200 py-2 px-4"
                     href="{{ route('contact-us-page') }}" @click="isOpen = false">Contact
                 </a>
             </li>
